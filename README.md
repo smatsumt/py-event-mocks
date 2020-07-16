0415e756b5445114eb9a1c6364ece2c24e81d370
Shinji Matsumoto <shinji.mtsmt@gmail.com>
2020-07-17 08:46:26 +0900

    Add document.

--- README.md
+++ README.md
@@ -10,3 +10,34 @@ pip install py-event-mocks
 
 ## Usage
 
+```python3
+from py_event_mocks import create_event
+
+event = create_event("aws:s3")
+# return 
+#   {
+#     "Records": [
+#       {
+#         "eventVersion": "2.0",
+#         "eventSource": "aws:s3",
+#         "awsRegion": "us-east-1",
+#     ...
+#   }
+
+event = create_event(
+    "aws:s3",
+    {
+      "Records": [{
+        "eventName": "ObjectCreated:Put",
+        "s3": {
+          "bucket": {
+            "name": "my-bucket-name"
+          },
+          "object": {
+            "key": "object-key"
+          }
+        }
+      }]
+    }
+)
+```
--- src/py_event_mocks/events.py
+++ src/py_event_mocks/events.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Events
+Events creation module
 """
 
 import json
@@ -19,10 +19,25 @@ EVENTS_JSON_PATH = SCRIPT_PATH / "events"
 
 def create_event(event_type: str, body: dict = None) -> dict:
     """
+    Return dict contents based on "event_type".
 
-    :param event_type:
-    :param body:
-    :return:
+    Allowed value for "event_type":
+        "aws:alexa-skill-event"
+        "aws:alexa-smart-home-event"
+        "aws:api-gateway-event"
+        "aws:cloud-watch-event"
+        "aws:cloud-watch-log-event"
+        "aws:cognito-user-pool-event"
+        "aws:dynamo-stream-event"
+        "aws:kinesis"
+        "aws:s3"
+        "aws:scheduled"
+        "aws:sns"
+        "aws:sqs"
+
+    :param event_type: Event type specification. i.e. "aws:s3", "aws:sns", ...
+    :param body: Additional body which includes return value
+    :return: Dict contents based on "event_type"
     """
     json_path = _event_type_to_json_path(event_type)
     with json_path.open() as fh:
@@ -33,9 +48,9 @@ def create_event(event_type: str, body: dict = None) -> dict:
 
 def _event_type_to_json_path(event_type: str) -> Path:
     """
-
+    Return .json file path for event_type
     :param event_type:
-    :return:
+    :return: Path object for event-type JSON file
     """
     event_path_str = event_type.replace(":", "/") + "-template.json"
     return EVENTS_JSON_PATH / event_path_str
